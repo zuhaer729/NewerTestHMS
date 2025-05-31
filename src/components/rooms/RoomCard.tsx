576b23e91f8d68ada50c71a5875b25f2ff6ced4b
zuhaer729 <zuhaer729@gmail.com>
2025-06-01 01:52:06 +0600

    Updated RoomCard.tsx

--- src/components/rooms/RoomCard.tsx
+++ src/components/rooms/RoomCard.tsx
@@ -42,7 +42,7 @@ const renderBookingInfo = () => {
 
   return (
     <div 
-      className={`card card-hover border ${getStatusColor()} p-3 min-h-[30px]`}
+      className={`card card-hover border ${getStatusColor()} p-3`}
       onClick={onClick}
       role="button"
       tabIndex={0}
